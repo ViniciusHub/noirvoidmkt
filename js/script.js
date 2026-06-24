884b713605ce2db3000505cbdd1b99411b8b612d
Vinicius Gustavo <41834774+ViniciusHub@users.noreply.github.com>
2026-06-23 22:19:26 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const cookie = document.querySelector('.fixed.bottom-0 button');
-  const bar = cookie?.closest('.fixed.bottom-0');
-  cookie?.addEventListener('click', () => bar?.remove());
+document.addEventListener('click', function (event) {
+  const target = event.target.closest('[data-scroll-top]');
+  if (target) {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+});
+
+const cookieBar = document.querySelector('.fixed.bottom-0');
+const cookieButton = cookieBar?.querySelector('button');
+cookieButton?.addEventListener('click', () => {
+  cookieBar.style.display = 'none';
 });
